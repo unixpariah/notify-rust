6d335cde850666a78aefc3ecafc4a87af3ab7b15
unixpariah <100892812+unixpariah@users.noreply.github.com>
2025-08-19 22:18:01 +0200

    feat: add nix flake with devshell for development

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Minimal Rust dev shell";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f (import nixpkgs { inherit system; }));
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = builtins.attrValues {
+            inherit (pkgs)
+              rustc
+              cargo
+              rust-analyzer-unwrapped
+              clippy
+              rustfmt
+              lldb
+              ;
+          };
+        };
+      });
+    };
+}
